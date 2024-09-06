745172cd3d3582dd7756a5c31522169fd3482a1b
Gerges_Hanna <Gerges_Hanna@DESKTOP-SHH46HM>
2024-09-06 03:05:15 +0200

    Make sql Connection is remotly string

--- src/pharmacy/v3/pkg0/sqlite.java
+++ src/pharmacy/v3/pkg0/sqlite.java
@@ -5,6 +5,9 @@
  */
 package pharmacy.v3.pkg0;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import javax.swing.JOptionPane;
@@ -15,11 +18,26 @@ import javax.swing.JOptionPane;
  */
 public class sqlite {
     Connection con=null;
+    public static String readConnectionPath() {
+        String connectionPath = null;
+        try (BufferedReader br = new BufferedReader(new FileReader("ConnectionPath.txt"))) {
+            connectionPath = br.readLine().trim(); // Read the first line of the file
+            System.out.println(connectionPath);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Error reading connection path file: " + e.getMessage());
+        }
+        return connectionPath;
+    }
     public static Connection connectDB()
     {
+        String connectionString=readConnectionPath();
+            if (connectionString == null || connectionString.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Connection string is not provided.");
+            return null;
+        }
         try {
             Class.forName("org.sqlite.JDBC");
-            Connection con=DriverManager.getConnection("jdbc:sqlite:pharmacy.db");
+            Connection con=DriverManager.getConnection(connectionString);
             
             return con;
             
